c2bf5820a231c00c9fedb5c2398a005dbd5583de
AJ <akarsh.1995.02@gmail.com>
2021-10-26 16:52:47 +0530

    bg fix

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <div
         className="flex flex-col h-full items-center justify-center backdrop-filter backdrop-blur-lg"
         style={{ display: adShow ? "" : "none" }}>
-        <div className="w-1/2 h-1/2 bg-black">
+        <div className="w-1/2 h-1/2">
           <Adsense
             client="ca-pub-5006765521801507"
             slot="6935948671"
