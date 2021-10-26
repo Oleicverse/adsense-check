98755ca37b78adc7e2d78300255d90d40fbca9de
AJ <akarsh.1995.02@gmail.com>
2021-10-26 14:43:21 +0530

    ad show and hide mechanism to test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import './App.css';
 import { Adsense } from '@ctrl/react-adsense';
+import { useState } from 'react';
+
+
+const CloseButton = ({ onClick }) => {
+  return (
+    <button className="border-2" onClick={onClick}>
+      Close
+    </button>
+  )
+}
+
+const ShowButton = ({ onClick }) => {
+  return (
+    <button className="border-2" onClick={onClick}>
+      Show
+    </button>
+  )
+}
 
 function App() {
+  const [adShow, setAdShow] = useState(true)
+
   return (
-    <div className="App h-screen">
-      <div className="flex flex-col h-full items-center justify-center">
-        <div>
+    <div className="App h-screen text-white"
+ style={{ background: 'url(https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png)' }}>
+      <ShowButton onClick={() => setAdShow(true)}/>
+      <div style={{ display: adShow ? "" : "none" }} className="flex flex-col h-full items-center justify-center backdrop-filter backdrop-blur-lg">
+        <div className="w-1/2 h-1/2">
           <Adsense
             client="ca-pub-5006765521801507"
             slot="6935948671"
-            style={{ width: 500, height: 300 }}
             format=""
+            className="w-1/2 h-1/2"
           />
         </div>
-
+        <CloseButton onClick={() => setAdShow(false)} />
       </div>
     </div>
   );
